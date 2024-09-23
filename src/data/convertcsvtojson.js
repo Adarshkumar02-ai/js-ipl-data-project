5421edee10cd17a69ecb26160a18cd5718c3f6a6
Adarshkumar02-ai <anishrj715@gmail.com>
2024-09-23 14:55:59 +0530

    Converted the CSV files into JSON format

---
+++ src/data/convertcsvtojson.js
@@ -0,0 +1,33 @@
+
+import csv from 'csvtojson' ;
+import fs from 'fs';
+let csvFilePath;
+let jsonFilePath;
+
+csvFilePath = './src/data/matches.csv';
+jsonFilePath = './src/data/matches.json';
+
+csv().fromFile(csvFilePath).then((jsonObj)=>{
+  try{
+    fs.writeFileSync(jsonFilePath, JSON.stringify(jsonObj,null,2));
+  }
+  catch(Error){
+    console.log(Error);
+    
+  }
+});
+
+
+let csvFilePath1 = './src/data/deliveries.csv';
+ let jsonFilePath1 = './src/data/deliveries.json';
+
+csv().fromFile(csvFilePath1).then((jsonObj)=>{
+  try{
+    fs.writeFileSync(jsonFilePath1, JSON.stringify(jsonObj,null,2));
+  }
+  catch(Error){
+    console.log(Error);
+    
+  }
+});
+ 
